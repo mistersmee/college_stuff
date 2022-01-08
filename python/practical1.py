5d31e15b97e5bc66f2db3170b93dd78df5c3886c
Aseem Athale <athaleaseem@gmail.com>
2022-01-08 09:00:29 +0530

    python: Add work from practical 1

---
+++ python/practical1.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+print("Hello world!")
+
+# In[3]:
+
+
+num1=30
+num2=20
+add=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+print("The addition of two numbers",num1,"and",num2,"is:",add)
+print("The subtraction of two numbers",num1,"and",num2," is:",sub)
+print("The multiplication of two numbers",num1,"and",num2," is:",mult)
+print("The division of two numbers",num1,"and",num2," is:",div)
+
+
+# In[10]:
+
+
+pi=3.14
+r=10
+area=pi*r*r
+print("The area of a circle with radius",r,"is:",area)
+
+
+# In[11]:
+
+
+b=100
+h=50
+area=0.5*b*h
+print("The area of a triangle with breadth",b,"and height",h,"is:",area)
+
+
+# In[15]:
+
+
+l=100
+area=l*l
+print("The area of a square with sides",l,"is:",area)
+
+
+# In[17]:
+
+
+l=10
+b=10
+area=l*b
+print("The area of a rectangle with length",l,"and breadth",b,"is:",area)
+
+
+
+# In[19]:
+
+
+num1=35
+num2=35
+num3=35
+num4=35
+num5=35
+avg=(num1+num2+num3+num4+num5)/5
+print("The average of 5 numbers",num1,",",num2,",",num3,",",num4,",",num5,"is:",avg)
+
+
+# In[21]:
+
+
+prin=2000
+rate=3
+yrs=3
+si=(prin*rate*yrs)/100
+print("The simple interest on a principal of",prin,"at the rate of",rate,"% and for a time period of",yrs,"years is:",si)
+
+
+# #PYTHON
+
+# In[22]:
+
+
+num1=30
+num2=60
+print("The value of number 1 is",num1, "and that of number 2 is",num2)
+temp=num1
+num1=num2
+num2=temp
+print("After interchange, the value of number 1 is",num1, "and that of number 2 is",num2)
+
+
+# In[24]:
+
+
+#Comment
+print("Thank You")
+
+
+# In[ ]:
