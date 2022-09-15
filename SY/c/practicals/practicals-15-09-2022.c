3fd8d35113e38d4bf2e452956176718e944e2fec
Aseem Athale <athaleaseem@gmail.com>
2022-09-15 10:42:29 +0530

    practicals: Add second program

--- SY/c/practicals/practicals-15-09-2022.c
+++ SY/c/practicals/practicals-15-09-2022.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 // Check if number is prime or not
 
 int
-main()
+prime()
 {
 	int num, flag = 0, i;
 	printf("Enter a number to check if prime or not:");
@@ -20,12 +21,38 @@ main()
 			break;
 		}
 	}
-	
+
 	if (flag != 0) {
 		printf("The number is composite.");
 	} else {
 		printf("The number is prime.");
 	}
 
+	return 0;
+}
+
+// Display the factorial of a given number
+
+int
+factorial()
+{
+	int num, i, fact = 1;
+
+	printf("Enter a number:");
+	scanf("%d", &num);
+
+	for (i = 1; i <= num; i++) {
+	    fact =  fact * i;
+	}
+	printf("The factorial of number %d is: %d", num, fact);
+
+	return 0;
+}
+
+int
+main()
+{
+	prime();
+	factorial();
 	return 0;
 }
