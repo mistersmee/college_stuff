7f418b4206b09ffee3ec3f06b2c260bf0c009278
Aseem Athale <athaleaseem@gmail.com>
2022-10-02 09:37:08 +0530

    java/assignments: Properly indent stuff

--- SY/java/assignments/assignment1.java
+++ SY/java/assignments/assignment1.java
@@ -30,18 +30,18 @@ public class assignment1 {
 		String dept = "AIML";
 		char division = 'C';
 		int rollno = 37;
-    int prn = 2122000421;
+		int prn = 2122000421;
 		float cgpi = 8.68f;
 
-	  System.out.println("Name is: " + name);
-    System.out.println("Department is: " + dept);
-    System.out.println("Division is: " + division);
-    System.out.println("Roll No. is: " + rollno);
-    System.out.println("PRN is: " + prn);
-    System.out.println("CGPI is: " + cgpi);
-  }
+		System.out.println("Name is: " + name);
+		System.out.println("Department is: " + dept);
+		System.out.println("Division is: " + division);
+		System.out.println("Roll No. is: " + rollno);
+		System.out.println("PRN is: " + prn);
+		System.out.println("CGPI is: " + cgpi);
+	}
 
-  public static void q3 () {
+	public static void q3 () {
 		int i = 9, j = 3;
 		assignment1 calc = new assignment1();
 		calc.Addition(i, j);
@@ -51,37 +51,37 @@ public class assignment1 {
 		System.out.println("The division of " + i + " and " + j + " is: " + divresult);
 	}
 
-  public static void q4() {
+	public static void q4() {
 
-    Scanner myObj = new Scanner(System.in);
+		Scanner myObj = new Scanner(System.in);
 
-    System.out.println("Enter Name:");
-    String name = myObj.nextLine();
-    System.out.println("Enter Department:");
-    String dept = myObj.nextLine();
-    System.out.println("Enter Division:");
-    String divisn = myObj.nextLine();
-    System.out.println("Enter Roll No:");
-    int rollno = myObj.nextInt();
-    System.out.println("Enter PRN:");
-    int prn = myObj.nextInt();
-    System.out.println("Enter CGPI:");
-    float cgpi = myObj.nextFloat();
+		System.out.println("Enter Name:");
+		String name = myObj.nextLine();
+		System.out.println("Enter Department:");
+		String dept = myObj.nextLine();
+		System.out.println("Enter Division:");
+		String divisn = myObj.nextLine();
+		System.out.println("Enter Roll No:");
+		int rollno = myObj.nextInt();
+		System.out.println("Enter PRN:");
+		int prn = myObj.nextInt();
+		System.out.println("Enter CGPI:");
+		float cgpi = myObj.nextFloat();
 
-    System.out.println("Name is: " + name);
-    System.out.println("Department is: " + dept);
-    System.out.println("Division is: " + divisn);
-    System.out.println("Roll No. is: " + rollno);
-    System.out.println("PRN is: " + prn);
-    System.out.println("CGPI is: " + cgpi);
+		System.out.println("Name is: " + name);
+		System.out.println("Department is: " + dept);
+		System.out.println("Division is: " + divisn);
+		System.out.println("Roll No. is: " + rollno);
+		System.out.println("PRN is: " + prn);
+		System.out.println("CGPI is: " + cgpi);
 
-    myObj.close();
-  }
+		myObj.close();
+	}
 
-  public static void main(String args[]) {
-    q1a();
-    q2();
-    q3();
-    q4();
-  }
+	public static void main(String args[]) {
+		q1a();
+		q2();
+		q3();
+		q4();
+	}
 }
