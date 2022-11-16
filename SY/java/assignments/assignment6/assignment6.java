82a75558b161c20836778edf3bd8b99764e2eb88
Aseem Athale <athaleaseem@gmail.com>
2022-11-16 10:12:39 +0530

    java/assignments: Modify assignment 6 work
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- SY/java/assignments/assignment6/assignment6.java
+++ SY/java/assignments/assignment6/assignment6.java
@@ -1,6 +1,38 @@
 public class assignment6 {
-    public static class Person {
 
+    public class IntArrayDemo {
+        public static void q1() {
+            int[] collegecode;
+            collegecode = new int[3];
+
+            collegecode[0] = 601;
+            collegecode[1] = 602;
+            collegecode[2] = 603;
+
+            for (int i = 0; i < collegecode.length; i++) {
+                System.out.println("College Code " + i
+                                   + " : " + collegecode[i]);
+            }
+        }
+    }
+
+    public class StringArrayDemo {
+        public static void q2() {
+            String[] colleges;
+            colleges = new String[3];
+
+            colleges[0] = "KIT";
+            colleges[1] = "DY";
+            colleges[2] = "SIT";
+
+            for (int i = 0; i < colleges.length; i++) {
+                System.out.println("College " + i
+                                   + " : " + colleges[i]);
+            }
+        }
+    }
+
+    public static class Person {
         public int age;
         public String name;
 
@@ -22,17 +54,27 @@ public class assignment6 {
         }
     }
 
-    public static void main(String args[]) {
+    public static void q3() {
+
+        Person James = new Person(21, "James");
+        Person Emma = new Person(22, "Emma");
+        Person John = new Person(77, "John");
 
         Person[] arr = new Person[3];
 
-        arr[0] = new Person(21, "James");
-        arr[1] = new Person(22, "Emma");
-        arr[2] = new Person(77, "John");
+        arr[0] = James;
+        arr[1] = Emma;
+        arr[2] = John;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Person " + i + " data: ");
             arr[i].display();
         }
+    }
+
+    public static void main(String args[]) {
+        IntArrayDemo.q1();
+        StringArrayDemo.q2();
+        q3();
     }
 }
