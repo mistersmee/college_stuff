608999fa14b36627b0d51d3a108d2ee99350bad7
Aseem Athale <athaleaseem@gmail.com>
2022-12-07 12:59:32 +0530

    java/assignmnent7: Modify uploaded assignment 7 work

---
+++ SY/java/assignments/assignment7/Studentv2.java
@@ -0,0 +1,12 @@
+//stduent class - version 1
+class Studentv2 extends Teacher{
+	int marks;
+	//setter methods to store marks
+	void setMarks(int marks){
+		this.marks = marks;
+	}
+	//getter method to get the marks
+	int getMarks(){
+		return marks;
+	}
+}
--- SY/java/assignments/assignment7/Use.java
+++ SY/java/assignments/assignment7/Use.java
@@ -1,13 +1,17 @@
 public class Use{
 	public static void main(String args[]){
 		//create object of Student
-		Student s = new Student();
-		s.setId(12);
-		s.setName("Smee");
-		s.setMarks(100);
+	//	Student s = new Student();
+	//	s.setId(12);
+	//	s.setName("Smee");
+	//	s.setMarks(100);
+		Studentv2 s2 = new Studentv2();
+		s2.setId(15);
+		s2.setName("Smee");
+		s2.setMarks(1000);
 		//retrieve data using getter methods and display
-		System.out.println("Id ="+s.getId());
-		System.out.println("Name ="+s.getName());
-		System.out.println("Marks ="+s.getMarks());
+		System.out.println("Id ="+s2.getId());
+		System.out.println("Name ="+s2.getName());
+		System.out.println("Marks ="+s2.getMarks());
 	}
 }
