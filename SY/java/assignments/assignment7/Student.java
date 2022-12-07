8bc11252ef576d79482b10b5c18874b38c6cf345
Aseem Athale <athaleaseem@gmail.com>
2022-12-07 12:06:08 +0530

    java/assignment7: Upload assignment 7 work [1/2]

---
+++ SY/java/assignments/assignment7/Student.java
@@ -0,0 +1,31 @@
+//stduent class - version 1
+class Student{
+	//instance variable
+	int id;
+	String name;
+	int marks;
+	//setter methods to store id
+	void setId( int id){
+		this.id=id;
+	}
+	//getter method to get the id
+	int getId(){
+		return id;
+	}
+	//setter methods to store name
+	void setName(String name){
+		this.name = name;
+	}
+	//getter method to get the name
+	String getName(){
+		return name;
+	}
+	//setter methods to store marks
+	void setMarks(int marks){
+		this.marks = marks;
+	}
+	//getter method to get the marks
+	int getMarks(){
+		return marks;
+	}
+}
