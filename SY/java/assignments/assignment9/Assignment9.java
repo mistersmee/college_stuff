1df7bed2d61b39ffc22f758c22c9b3c30b04187e
Aseem Athale <athaleaseem@gmail.com>
2022-12-24 15:04:05 +0530

    java/assignments: Upload assignment 9 work
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- SY/java/assignments/assignment9/Assignment9.java
+++ SY/java/assignments/assignment9/Assignment9.java
@@ -1,39 +1,169 @@
-import java.util.Scanner;
+import java.util.*;
+
+abstract class Person {
+    int Id;
+    String Name;
+    abstract public int getManagerID();
+    abstract void displayE();
+    abstract void displayM();
+}
+
+abstract class Employee extends Person {
+
+    String Post;
+	int Salary;
+	int ManagerID;
+   	boolean IsManager;
+
+	public Employee() {
+       	IsManager = false;
+   	}
+
+   	public int getManagerID() {
+   	    return ManagerID;
+   	}
+
+   	void displayE() {
+		IsManager = false;
+		System.out.println("*EMPLOYEE*");
+		System.out.println("ID = "+ Id);
+       	System.out.println("Name = "+ Name);
+        System.out.println("Post = " + Post);
+        System.out.println("Salary = " + Salary);
+        System.out.println("ManagerID  = " + ManagerID);
+        System.out.println("IsManager = " + IsManager);
+	}
+}
+
+class Manager extends Employee {
+
+    String Department;
+
+	public Manager() {
+        ManagerID = 0;
+        IsManager = true;
+	}
+
+    public int getManagerID() {
+    	return ManagerID;
+    }
+
+    void displayM() {
+		System.out.println("*MANAGER*");
+        System.out.println("Id = " + Id);
+		System.out.println("Name = " + Name);
+        System.out.println("Post = " + Post);
+        System.out.println("Salary = " + Salary);
+        System.out.println("ManagerID  = " + ManagerID);
+        System.out.println("IsManager = " + IsManager);
+        System.out.println("Department = " + Department);
+    }
+}
 
 public class Assignment9 {
-    class Person {
-        int Id;
-        String Name;
-    }
-    
-    class Employee extends Person{
-        String Post;
-        int Salary;
-        int ManagerID;
-        boolean IsManager;
-    }
-    
-    class Manager extends Employee{
-        String Department;
-    }
 
-    public void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
 
-        char keepgoing = 'y';
-        int i, j = 0;
+		Person p[] = new Person[10];
+		Scanner s = new Scanner(System.in);
+		int c = 0;
+		while(true) {
+			int choice = s.nextInt();
 
-        System.out.println("HR Management system:");
-        System.out.println("Choose what you want to do:");
-        System.out.println("\t1.Create Manager\n\t2.Create Employee\n\t3.Display\n\t4. Exit");
-        
-        while (keepgoing == 'y') {
-            System.out.println("Enter your choice:");
-            int choice = sc.nextInt();
-            switch(choice) {
-                case 1:
-                    
-            }
-        }
-    }
+			switch(choice) {
+
+				case 1:
+
+
+				Manager m_obj = new Manager();
+				p[c] = m_obj;
+
+				m_obj.Id = c;
+				System.out.println("Enter Name : ");
+				m_obj.Name = s.next();
+				System.out.println("Enter Post : ");
+				m_obj.Post = s.next();
+				System.out.println("Enter Salary : ");
+				m_obj.Salary = s.nextInt();
+				System.out.println("Enter Department : ");
+				m_obj.Department = s.nextLine();
+				c++;
+				break;
+
+				case 2:
+
+					Employee obj = new Manager();
+					p[c] = obj;
+
+					obj.Id = c;
+					System.out.println("Enter Name : ");
+					obj.Name = s.next();
+					System.out.println("Enter Post : ");
+					obj.Post = s.nextLine();
+					System.out.println("Enter Salary : ");
+					obj.Salary = s.nextInt();
+					System.out.println("Enter ManagerID : ");
+					obj.ManagerID = s.nextInt();
+					c++;
+				break;
+
+				case 3:
+					System.out.println("1. Display All Employee\n2. Display Manager\n3. Display Employes by MangerID");
+                    int ch = s.nextInt();
+
+                    switch(ch)
+        			{
+            			case 1:
+            				for(int i=0;i<c;i++)
+                            {
+								if(p[i].getManagerID()==0) {
+									p[i].displayM();
+								}
+								else {
+									p[i].displayE();
+								}
+						    }
+                    	break;
+
+                        case 2:
+                        	for(int i=0;i<c;i++)
+                        	{
+                            	if(p[i].getManagerID() == 0)
+                            	{
+                                	p[i].displayM();
+                            	}
+                        	}
+                        break;
+
+                        case 3:
+                			System.out.println("Enter ManagerID : ");
+            				int id_m = s.nextInt();
+							int cnt=0;
+                			for(int i=0;i<c;i++)
+                        	{
+								if(p[i].getManagerID()== id_m)
+                				{
+            						p[i].displayE();
+									cnt++;
+                            	}
+                        	}
+
+							if(cnt==0) {
+								System.out.println("not a Manager");
+							}
+
+                		break;
+
+						default :
+							System.out.println("Invalid Choice");
+					}
+				break;
+
+				default :
+					System.out.println("Invalid Choice");
+
+			}
+			s.close();
+		}
+	}
 }
