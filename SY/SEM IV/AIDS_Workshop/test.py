e9882c8e1b5f6a6ffe0d0976d063a7bf79a03caa
Aseem Athale <athaleaseem@gmail.com>
2023-03-03 14:44:47 +0530

    SY: Upload workshop work and practical work
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

---
+++ SY/SEM IV/AIDS_Workshop/test.py
@@ -0,0 +1,19 @@
+a = int(10)
+b = str("hello")
+c = (100.55)
+d = "a"
+
+list1 = ["Hello", "Hi", "Ola", "Mamma mia"]
+tuple1 = ("smee", "aseem", "meesa", "athale")
+
+dictionary1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(a, b, c, d)
+
+print(list1, tuple1, dictionary1)
+
+print(list1[1:3:1])
