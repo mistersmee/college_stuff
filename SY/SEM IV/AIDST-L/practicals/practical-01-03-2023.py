0dcc7b2378cef3219b0586e5cd39c7197b817169
Aseem Athale <athaleaseem@gmail.com>
2023-03-04 13:58:48 +0530

    AIDST-L: Upload 1/3/2023 work

---
+++ SY/SEM IV/AIDST-L/practicals/practical-01-03-2023.py
@@ -0,0 +1,56 @@
+# Write a Python program to read a list of words and return the length of longest word
+# WAP to create dictionary as student which has the keys: name, PRN, marks, phone number, Dynamic, compute the average marks of ten students
+# WAP TO CREATE A DICTIONEAYR WIRH EYS AS FIERT CHARAGERTTE AND VALEU AS WORDS STARTING WIRTHT THEAT CHARAGECETE
+
+# Program 1
+
+def longestLength(a):
+    max1 = len(a[0])
+    temp = a[0]
+ 
+    # for loop to traverse the list
+    for i in a:
+        if(len(i) > max1):
+ 
+            max1 = len(i)
+            temp = i
+ 
+    print("The word with the longest length is:", temp,
+          " and length is ", max1)
+ 
+ 
+# Driver Program
+a = []
+
+k = 'y'
+
+while k == 'y':
+    a.append(input("Enter a word:"))
+    k = input("Do you want to enter more words? [y/n]:")
+
+longestLength(a)
+
+# Program 2
+
+d = {}
+i = 0
+avg = 0
+
+while i < 10:
+    print(f"Enter the details for student: {i+1}")
+    d[0] = input("Enter student name:")
+    d[1] = input("Enter student PRN:")
+    d[2] = int(input("Enter student total marks:"))
+    d[3] = input("Enter student phone number:")
+    avg = avg + d[2]
+    i += 1
+
+avg = avg / 10
+
+print("The average of 10 students marks is" + avg)
+
+
+# Program 3
+
+alphabet = {}
+
